395de32d61c11d586d5317cd25ca06b10cacfa74
fremag <fremag.gh@gmail.com>
2019-12-16 20:13:51 +0100

    Menger sponge: restore demo scene

--- ray-tracer-demos/MengerSpongeScene.cs
+++ ray-tracer-demos/MengerSpongeScene.cs
@@ -8,19 +8,22 @@ namespace ray_tracer_demos
         public MengerSpongeScene()
         {
             CameraParameters.Clear();
-            CameraParameters.Add(new CameraParameters{Name = "Default", Width = 1280, Height = 1024, CameraX = 0, CameraY = 2, CameraZ = -2, LookX = 0, LookY = 0, LookZ = -1});
+            CameraParameters.Add(new CameraParameters{Name = "Default", Width = 640, Height = 400, CameraX = 3, CameraY = 2, CameraZ = -12, LookX = -1.5, LookY = 1, LookZ = 0});
         }
 
         public override void InitWorld()
         {
             DefaultFloor().Translate(ty: -1.5);
-            Light(-1, 5, -5, Color.White/2);
-            Light(-5, 5, -1, Color.White/2);
+            DefaultFloor().Translate(ty: -1.5);
+            Light(0, 0, 0, Color.White);
+            Light(0, 5, -20, Color.White/2);
+            Light(10, 5, 0, Color.White/4);
+            Light(0, 20, 0, Color.White/3);
 
-            var sponge2 = new MengerSponge(2);
-            var cube = new Cube().Scale(sx: 2, sz: 2).Translate(ty: 1);
-            var diff = new CsgDifference(sponge2, cube);
-            Add(diff);
+            var sponge0 = new MengerSponge(0).Translate(tx: -6);
+            var sponge1 = new MengerSponge(1).Translate(tx: -2);
+            var sponge2 =  new MengerSponge(2).Translate(tx: 2);
+            Add(sponge0, sponge1, sponge2);
         }
    }
 }
