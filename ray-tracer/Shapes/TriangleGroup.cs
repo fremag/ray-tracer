23ea762152221a82e773657ce11f0b9c4dada5e6
fremag <fremag.gh@gmail.com>
2020-04-09 17:32:44 +0200

    TriangleGroup: specialized group for triangles Keep old non AVX code for benchmarks

--- ray-tracer/Shapes/TriangleGroup.cs
+++ ray-tracer/Shapes/TriangleGroup.cs
@@ -1,3 +1,4 @@
+#define AVX
 #define OPTIM
 using System;
 using System.Collections.Concurrent;
@@ -5,7 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.Intrinsics;
+#if AVX
 using System.Runtime.Intrinsics.X86;
+#endif
 
 namespace ray_tracer.Shapes
 {
@@ -126,7 +129,7 @@ namespace ray_tracer.Shapes
                 e2_Z[i] = transformedE2.Z;
             }
         }
-
+#if AVX
         public unsafe void IntersectAll(ref Tuple origin, ref Tuple rayDir, Intersections intersections)
         {
             var count = p1_X.Length;
@@ -302,7 +305,104 @@ namespace ray_tracer.Shapes
                 intersections.Add(intersection);
             }
         }
+#else
+       public unsafe void IntersectAll(ref Tuple origin, ref Tuple rayDir, Intersections intersections)
+        {
+            var count = Triangles.Count;
+            var skip = stackalloc bool[count];
+            var dirCrossE2_X = stackalloc float[count];
+            var dirCrossE2_Y = stackalloc float[count];
+            var dirCrossE2_Z = stackalloc float[count];
+            var det = stackalloc float[count];
+            
+            var rayDirX = (float)rayDir.X;
+            var rayDirY = (float)rayDir.Y;
+            var rayDirZ = (float)rayDir.Z;
 
+            for (int i = 0; i < count; i++)
+            {
+                dirCrossE2_X[i] = rayDirY * e2_Z[i] - rayDirZ * e2_Y[i];
+                dirCrossE2_Y[i] = rayDirZ * e2_X[i] - rayDirX * e2_Z[i];
+                dirCrossE2_Z[i] = rayDirX * e2_Y[i] - rayDirY * e2_X[i];
+
+                det[i] = e1_X[i] * dirCrossE2_X[i] + e1_Y[i] * dirCrossE2_Y[i] + e1_Z[i] * dirCrossE2_Z[i];
+            }
+
+            float originX = (float)origin.X;
+            float originY = (float)origin.Y;
+            float originZ = (float)origin.Z;
+            var u = stackalloc float[count];
+            var f = stackalloc float[count];
+            var p1ToOrigin_X = stackalloc float[count];
+            var p1ToOrigin_Y = stackalloc float[count];
+            var p1ToOrigin_Z = stackalloc float[count];
+            bool skipAll = true;
+            for (int i = 0; i < count; i++)
+            {
+                if (Math.Abs(det[i]) < Helper.Epsilon)
+                {
+                    skip[i] = true;
+                    continue;
+                }
+
+                skipAll = false;
+                skip[i] = false;
+                f[i] = 1.0f / det[i];
+                p1ToOrigin_X[i] = originX - p1_X[i];
+                p1ToOrigin_Y[i] = originY - p1_Y[i];
+                p1ToOrigin_Z[i] = originZ - p1_Z[i];
+
+                float uu = p1ToOrigin_X[i] * dirCrossE2_X[i];
+                uu += p1ToOrigin_Y[i] * dirCrossE2_Y[i];
+                uu += p1ToOrigin_Z[i] * dirCrossE2_Z[i];
+                u[i] = f[i] *uu;
+            }
+
+            if (skipAll)
+            {
+                return;
+            }
+
+            skipAll = true;
+            var v = stackalloc float[count];
+            var originCrossE1_X = stackalloc float[count];
+            var originCrossE1_Y = stackalloc float[count];
+            var originCrossE1_Z = stackalloc float[count];
+            for (int i = 0; i < count; i++)
+            {
+                if (u[i] < 0 || u[i] > 1)
+                {
+                    skip[i] = true;
+                    continue;
+                }
+
+                skipAll = false;
+                originCrossE1_X[i] = p1ToOrigin_Y[i] * e1_Z[i] - p1ToOrigin_Z[i] * e1_Y[i];
+                originCrossE1_Y[i] = p1ToOrigin_Z[i] * e1_X[i] - p1ToOrigin_X[i] * e1_Z[i];
+                originCrossE1_Z[i] = p1ToOrigin_X[i] * e1_Y[i] - p1ToOrigin_Y[i] * e1_X[i];
+
+                v[i] = f[i] * (rayDirX * originCrossE1_X[i] + rayDirY * originCrossE1_Y[i] + rayDirZ * originCrossE1_Z[i]);
+            }
+
+            if (skipAll)
+            {
+                return;
+            }
+            
+            for (int i = 0; i < count; i++)
+            {
+                if (skip[i] || v[i] < 0 || (u[i] + v[i]) > 1)
+                {
+                    continue;
+                }
+
+                var t = f[i] * (e2_X[i] * originCrossE1_X[i] + e2_Y[i] * originCrossE1_Y[i] + e2_Z[i] * originCrossE1_Z[i]);
+                var intersection = new Intersection(t, Triangles[i] , u[i], v[i]);
+                intersections.Add(intersection);
+            }
+        }
+#endif
+        
         public override Tuple NormalAtLocal(Tuple worldPoint, Intersection hit = null)
         {
             throw new InvalidOperationException();
