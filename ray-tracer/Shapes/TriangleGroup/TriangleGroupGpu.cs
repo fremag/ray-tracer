172cd1584dbf4b669f238d17354b5302b34fa7e1
fremag <fremag.gh@gmail.com>
2020-04-28 09:03:19 +0200

    Triangles / GPU: add new RayData struct

--- ray-tracer/Shapes/TriangleGroup/TriangleGroupGpu.cs
+++ ray-tracer/Shapes/TriangleGroup/TriangleGroupGpu.cs
@@ -4,6 +4,26 @@ using System.Numerics;
 
 namespace ray_tracer.Shapes.TriangleGroup
 {
+    public readonly struct RayData
+    {
+        public float X { get; } 
+        public float Y{ get; }
+        public float Z{ get; } 
+        public float DirX{ get; } 
+        public float DirY{ get; } 
+        public float DirZ{ get; }
+
+        public RayData(float x, float y, float z, float dirX, float dirY, float dirZ)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            DirX = dirX;
+            DirY = dirY;
+            DirZ = dirZ;
+        }
+    }
+
     public readonly struct TriangleData
     {
         public float p1_X { get; }
@@ -40,12 +60,12 @@ namespace ray_tracer.Shapes.TriangleGroup
         public float originCrossE1_Y { get; set; }
         public float originCrossE1_Z { get; set; }
     }
-    
+
     public class TriangleGroupGpu : AbstractTriangleGroup
     {
         private bool cached = false;
         private TriangleData[] triangleData;
-
+        
         public TriangleGroupGpu()
         {
         }
@@ -108,16 +128,11 @@ namespace ray_tracer.Shapes.TriangleGroup
         {
             var count = Triangles.Count;
             var triangleResult = stackalloc TriangleResult[count];
-            var rayDirX = (float) direction.X;
-            var rayDirY = (float) direction.Y;
-            var rayDirZ = (float) direction.Z;
-            float originX = (float) origin.X;
-            float originY = (float) origin.Y;
-            float originZ = (float) origin.Z;
+            var ray = new RayData((float) origin.X, (float) origin.Y, (float) origin.Z, (float) direction.X, (float) direction.Y, (float) direction.Z);
 
             for (int i = 0; i < count; i++)
             {
-                Compute(i, ref triangleData, ref triangleResult, originX, originY, originZ, rayDirX, rayDirY, rayDirZ);
+                Compute(i, ref triangleData, ref triangleResult, ref ray);
             }
 
             for (int i = 0; i < count; i++)
@@ -134,22 +149,22 @@ namespace ray_tracer.Shapes.TriangleGroup
             }
         }
 
-        private unsafe void Compute(in int i, ref TriangleData[] triangleDatas, ref TriangleResult* triangleResult, in float originX, in float originY, in float originZ, in float rayDirX, in float rayDirY, in float rayDirZ)
+        private static unsafe void Compute(in int i, ref TriangleData[] triangleDatas, ref TriangleResult* triangleResult, ref RayData ray)
         {
             var result = triangleResult[i];
             var dataTriangle = triangleDatas[i];
             
-            float dirCrossE2_X = rayDirY * dataTriangle.e2_Z - rayDirZ * dataTriangle.e2_Y;
-            float dirCrossE2_Y = rayDirZ * dataTriangle.e2_X - rayDirX * dataTriangle.e2_Z;
-            float dirCrossE2_Z = rayDirX * dataTriangle.e2_Y - rayDirY * dataTriangle.e2_X;
+            float dirCrossE2_X = ray.DirY * dataTriangle.e2_Z - ray.DirZ * dataTriangle.e2_Y;
+            float dirCrossE2_Y = ray.DirZ * dataTriangle.e2_X - ray.DirX * dataTriangle.e2_Z;
+            float dirCrossE2_Z = ray.DirX * dataTriangle.e2_Y - ray.DirY * dataTriangle.e2_X;
 
             float det = dataTriangle.e1_X * dirCrossE2_X + dataTriangle.e1_Y * dirCrossE2_Y + dataTriangle.e1_Z * dirCrossE2_Z;
 
             result.f = 1.0f / det;
                 
-            float p1ToOrigin_X = originX - dataTriangle.p1_X;
-            float p1ToOrigin_Y = originY - dataTriangle.p1_Y;
-            float p1ToOrigin_Z = originZ - dataTriangle.p1_Z;
+            float p1ToOrigin_X = ray.X - dataTriangle.p1_X;
+            float p1ToOrigin_Y = ray.Y - dataTriangle.p1_Y;
+            float p1ToOrigin_Z = ray.Z - dataTriangle.p1_Z;
 
             result.u = result.f * (p1ToOrigin_X * dirCrossE2_X + p1ToOrigin_Y * dirCrossE2_Y + p1ToOrigin_Z * dirCrossE2_Z);
                  
@@ -157,7 +172,7 @@ namespace ray_tracer.Shapes.TriangleGroup
             result.originCrossE1_Y = p1ToOrigin_Z * dataTriangle.e1_X - p1ToOrigin_X * dataTriangle.e1_Z;
             result.originCrossE1_Z = p1ToOrigin_X * dataTriangle.e1_Y - p1ToOrigin_Y * dataTriangle.e1_X;
 
-            result.v = result.f * (rayDirX * result.originCrossE1_X + rayDirY * result.originCrossE1_Y + rayDirZ * result.originCrossE1_Z);
+            result.v = result.f * (ray.DirX * result.originCrossE1_X + ray.DirY * result.originCrossE1_Y + ray.DirZ * result.originCrossE1_Z);
 
             result.skip = MathF.Abs(det) < Helper.Epsilon || result.u < 0 || result.u > 1 || result.v < 0 || (result.u + result.v) > 1;
             triangleResult[i] = result;
